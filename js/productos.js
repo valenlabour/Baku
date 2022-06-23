529e9cb264767ed84292433abe83bbaff353fc96
VaÃlentin Labour <valenlabour@gmail.com>
2022-06-23 18:44:30 -0300

    Entrega eventos

---
+++ js/productos.js
@@ -0,0 +1,87 @@
+const productos = [
+    {
+        categoria: 'remeras',
+        nombre: 'Remera Fear',
+        precio: 3499,
+        img: "../img/remeras/remera-fear.jpg"
+    },
+    {
+        categoria: 'remeras',
+        nombre: 'Remera Stylver',
+        precio: 3499,
+        img: "../img/remeras/remera-full-black.jpg"
+    },
+    {
+        categoria: 'remeras',
+        nombre: 'Remera Good Life',
+        precio: 3499,
+        img: "../img/remeras/remera-good-life.jpg"
+    },
+    {
+        categoria: 'remeras',
+        nombre: 'Remera Arcano',
+        precio: 3499,
+        img: "../img/remeras/remera-arcano.jpg"
+    },
+    {
+        categoria: 'buzos',
+        nombre: 'Buzo Rider',
+        precio: 6699,
+        img: "../img/buzos/buzo-rider.jpg"
+    },
+    {
+        categoria: 'buzos',
+        nombre: 'Buzo Nigo Grey',
+        precio: 7299,
+        img: "../img/buzos/buzo-nigo-grey.jpg"
+    },
+    {
+        categoria: 'buzos',
+        nombre: 'Buzo Walo Pink',
+        precio: 7299,
+        img: "../img/buzos/buzo-walo-pink.jpg"
+    },
+    {
+        categoria: 'buzos',
+        nombre: 'Buzo Nigo Vison',
+        precio: 7299,
+        img: "../img/buzos/buzo-nigo-vison.jpg"
+    },
+    {
+        categoria: 'buzos',
+        nombre: 'Buzo Future',
+        precio: 6699,
+        img: "../img/buzos/buzo-future.jpg"
+    },
+    {
+        categoria: 'buzos',
+        nombre: 'Buzo Sloom',
+        precio: 6699,
+        img: "../img/buzos/buzo-sloom.jpg"
+    },
+    {
+        categoria: 'pantalones',
+        nombre: 'Jean Cropped',
+        precio: 7240,
+        img: "../img/pantalones/jean-cropped.jpg"
+    },
+    
+    {
+        categoria: 'pantalones',
+        nombre: 'Jean Charles',
+        precio: 7890,
+        img: "../img/pantalones/jean-charles.jpg"
+    },
+    {
+        categoria: 'pantalones',
+        nombre: 'Jean Tyler',
+        precio: 8900,
+        img: "../img/pantalones/jean-tyler.jpg"
+    },
+    {
+        categoria: 'pantalones',
+        nombre: 'Jogger Rodd Navy',
+        precio: 6200,
+        img: "../img/pantalones/jogger-rodd-navy.jpg"
+    },
+]
